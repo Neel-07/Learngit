818df5429e67014f3a88d09f13d06287f05ae291
Neel Mishra <neelmishra9125@gmail.com>
2023-08-19 21:35:19 +0530

    final commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class sum{
+    public static void main (String args[]){
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        a=sc.nextInt();
+        System.out.println("Enter the second number");
+        b=sc.nextInt();
+        System.out.println(a+b);
+    }
+}
